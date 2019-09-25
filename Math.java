506b9903cc7c9217935e62a7ea7a4251ad9357c0
Jarod Peachey <jwpeachey107@aol.com>
2019-09-25 15:44:08 -0400

    Add main

---
+++ Math.java
@@ -0,0 +1,14 @@
+
+/**
+ * The purpose of this program is to do math
+ * 
+ * @author Jarod Peachey
+ * @version 9/12/19
+ */
+
+public class Math {
+  public static void main(String[] args) {
+    int iNum = 6.23;
+    System.out.println(iNum);
+  }// end of main method
+}// end of class
