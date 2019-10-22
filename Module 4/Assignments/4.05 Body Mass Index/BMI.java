48ca755f366ed997ba1f69027a40931aea5dc94d
Jarod Peachey <jwpeachey107@aol.com>
2019-10-22 10:11:40 -0400

    Updates

--- Module 4/Assignments/4.05 Body Mass Index/BMI.java
+++ Module 4/Assignments/4.05 Body Mass Index/BMI.java
@@ -15,8 +15,16 @@ import java.text.DecimalFormat;
 
 public class BMI {
   static double userBMI = 0.0;
+  static double userWeight = 0.0;
+  static double userHeight = 0.0;
+
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
+
+    System.out.print("Enter your first and last name:  ");
+    String fullName = in.next();
+    fullName += in.nextLine();
+    System.out.println("");
 
     System.out.print("What is your height? (FEET INCHES)  ");
     String height = in.next();
@@ -29,33 +37,46 @@ public class BMI {
 
     calculateBMI(height, inches, weight);
 
-    DecimalFormat formatUserBMI = new DecimalFormat("#.00");
+    DecimalFormat formatDecimal = new DecimalFormat("#.00");
 
-    String finalBMI = formatUserBMI.format(userBMI);
+    String finalBMI = formatDecimal.format(userBMI);
+    String finalHeight = formatDecimal.format(userHeight);
+    String finalWeight = formatDecimal.format(userWeight);
 
-    System.out.println("Your BMI is " + formatUserBMI.format(userBMI));
-    System.out.println("");
+    String userWeightStatus = getUserWeightStatus(Double.parseDouble(finalBMI));
 
+    System.out.println("~~~~~~~~ BMI Calculator Results ~~~~~~~~~~~");
+    System.out.println();
+    System.out.println("Name: " + fullName);
+    System.out.println("Height: " + finalHeight + " (meters)");
+    System.out.println("Weight: " + finalWeight + " (kilograms)");
+    System.out.println("BMI: " + finalBMI);
+    System.out.println("Weight Status: " + userWeightStatus);
+  }// end of main method
+
+  static void calculateBMI(String height, String inches, int weight) {
+    double wholeFeet = Double.parseDouble(height) + (Double.parseDouble(inches) / 12);
+    double calculatedUserHeight = wholeFeet / 3.28;
+    double calculatedUserWeight = (double) (weight) / 2.2046;
+    double calculatedUserBMI = calculatedUserWeight / (double) Math.pow(calculatedUserHeight, 2);
+
+    userBMI = calculatedUserBMI;
+    userWeight = calculatedUserWeight;
+    userHeight = calculatedUserHeight;
+  }
+
+  static String getUserWeightStatus (double BMI) {
     String weightStatus = "";
 
-    if (Double.parseDouble(finalBMI) < 18.5)
+    if (BMI < 18.5)
       weightStatus = "Underweight";
-    else if (Double.parseDouble(finalBMI) < 24.5)
+    else if (BMI < 24.5)
       weightStatus = "Normal";
-    else if (Double.parseDouble(finalBMI) < 30)
+    else if (BMI < 30)
       weightStatus = "Overweight";
     else
       weightStatus = "Obese";
 
-    System.out.println("Your weight status is: \"" + weightStatus + "\"");
-  }// end of main method
-
-  static void calculateBMI(String height, String inches, int weight) {
-    double wholeFeet = Double.parseDouble(height) +  ( Double.parseDouble(inches) / 12);
-    double heightInMeters = wholeFeet / 3.28;
-    double weightInKilos =  (double)(weight) / 2.2046;
-    double calculatedUserBMI = weightInKilos / (double)Math.pow(heightInMeters, 2);
-
-    userBMI = calculatedUserBMI;
+    return weightStatus;
   }
 }// end of class
