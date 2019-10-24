594df454cfcfd219d4b437abbe8bc2f6800ad3dd
Jarod Peachey <jwpeachey107@aol.com>
2019-10-24 11:23:40 -0400

    More tests

--- Math.java
+++ Math.java
@@ -10,22 +10,11 @@ import java.util.Scanner;
 
 public class Math {
   public static void main(String[] args) {
-    System.out.println("Weight (kg): " + (int) (10 * 135) / 10.0);
-    System.out.println("Height (cm): " + (int) (10 * 200) / 10.0);
-    System.out.println("Basal Metabolic Rate: " + (int) (10 * 12) / 10.0);
-
-    String genderString = "female";
-    String ageString = "12";
-    char gender = genderString.indexOf('0');
-    boolean isFemale = gender == "F";
-    int age = Integer.parseInt(ageString);
-
-    System.out.println("Is female: " + isFemale);
-    System.out.println("Age:" + age);
+    System.out.print("CHRISTI SMITH".compareTo("Christi Smith") < 0);
   }// end of main method
 }// end of class
 
 /*
- javac Math.java
- java Math
+javac Math.java
+java Math
  */
