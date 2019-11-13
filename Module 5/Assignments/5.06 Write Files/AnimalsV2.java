046b6ce82853a924c125ba8b7a52bf858c66d208
Jarod Peachey <jwpeachey107@aol.com>
2019-11-12 20:59:45 -0500

    Update 5.06 Assignment

--- Module 5/Assignments/5.06 Write Files/AnimalsV2.java
+++ Module 5/Assignments/5.06 Write Files/AnimalsV2.java
@@ -12,8 +12,9 @@ import java.io.PrintWriter;
 import java.io.File;
 import java.lang.Math;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
-class Animals {
+class AnimalsV2 {
   public static void main(String[] args) throws IOException {
     PrintWriter outFile = new PrintWriter(new File("animal_data.txt"));
     String format = "%1$-20s%2$20s\n";
@@ -22,40 +23,30 @@ class Animals {
 
     for (int i = 1; i <= trials; i++) {
       int foxSquirrelsSeenInTrial = 0;
-      int graySquirrelsSeenInTrial = 0;
-      int redSquirrelsSeenInTrial = 0;
-      int totalSquirrelsSeenInTrial = 0;
+      int totalAttempts = 0;
 
       while (foxSquirrelsSeenInTrial < 10) {
         int num = (int) (Math.random() * 10) + 1;
-        if (num >= 1 && num <= 5) {
-          graySquirrelsSeenInTrial++;
-        } else if (num <= 9) {
-          redSquirrelsSeenInTrial++;
-        } else {
+        if (num == 5) {
           foxSquirrelsSeenInTrial++;
         }
 
-        totalSquirrelsSeenInTrial++;
+        totalAttempts++;
       }
 
-      outFile.println("Trial " + i);
-      outFile.format(format, "Gray Squirrels: ", graySquirrelsSeenInTrial);
-      outFile.format(format, "Red Squirrels: ", redSquirrelsSeenInTrial);
-      outFile.format(format, "Fox Squirrels: ", foxSquirrelsSeenInTrial);
-      outFile.format(format, "", "Total: " + totalSquirrelsSeenInTrial);
-      outFile.println();
+      outFile.println(" ");
+      outFile.println("Trial " + i + " --------------------------------");
+      outFile.format(format, "", "Total Attempts: " + totalAttempts);
+      outFile.format(format, "", "Fox Squirrels: " + foxSquirrelsSeenInTrial);
     }
     outFile.close();
 
     String nextLineToken = "";
-    String nextToken = "";
     File fileName = new File("animal_data.txt");
     Scanner inFile = new Scanner(fileName);
 
     int totalFoxSquirrels = 0;
-    int totalGraySquirrels = 0;
-    int totalRedSquirrels = 0;
+    int totalAttempts = 0;
 
     // while loop will continue while inFile has a next token to read
     while (inFile.hasNext()) {
@@ -79,7 +70,7 @@ class Animals {
             totalFoxSquirrels += finalDigit;
           }
         }
-      } else if (nextLineToken.contains("Gray Squirrels")) {
+      } else if (nextLineToken.contains("Attempts")) {
         for (int i = 0; i < nextLineToken.length(); i++) {
           if (i != nextLineToken.length() - 1 && Character.isDigit(nextLineToken.charAt(i))
               && Character.isDigit(nextLineToken.charAt(i + 1))) {
@@ -87,39 +78,21 @@ class Animals {
             String secondDigit = String.valueOf(nextLineToken.charAt(i + 1));
             int finalDigit = Integer.parseInt(firstDigit + secondDigit);
 
-            totalGraySquirrels += finalDigit;
-          }
-        }
-      } else if (nextLineToken.contains("Red Squirrels")) {
-        for (int i = 0; i < nextLineToken.length(); i++) {
-          if (i != nextLineToken.length() - 1 && Character.isDigit(nextLineToken.charAt(i))
-              && Character.isDigit(nextLineToken.charAt(i + 1))) {
-            String firstDigit = String.valueOf(nextLineToken.charAt(i));
-            String secondDigit = String.valueOf(nextLineToken.charAt(i + 1));
-            int finalDigit = Integer.parseInt(firstDigit + secondDigit);
-
-            totalRedSquirrels += finalDigit;
+            totalAttempts += finalDigit;
           }
         }
       }
-
-      if (containsWhitespace) {
-        System.out.println(nextLineToken);
-      } else {
-        System.out.print(nextLineToken);
-      }
+      System.out.println(nextLineToken);
     } // end while
 
-    int totalSquirrels = totalFoxSquirrels + totalGraySquirrels + totalRedSquirrels;
+    DecimalFormat df = new DecimalFormat("#.00");
 
     System.out.println();
+    System.out.format(format, "Total Attempts: ", totalAttempts);
     System.out.format(format, "Fox Squirrels: ", totalFoxSquirrels);
-    System.out.format(format, "Gray Squirrels: ", totalGraySquirrels);
-    System.out.format(format, "Red Squirrels: ", totalRedSquirrels);
-    System.out.format(format, "Total Squirrels: ", totalRedSquirrels + totalFoxSquirrels + totalGraySquirrels);
-    System.out.println();
-    System.out.println(
-        "The total percentage of fox squirrels compared to all others is " + totalSquirrels / totalFoxSquirrels + "%");
+    System.out.format(format,
+    "Percentage:", df.format((double)((double)totalFoxSquirrels / (double)totalAttempts) * 100.0)
+     + "%");
 
     inFile.close();
   }// end of main method
