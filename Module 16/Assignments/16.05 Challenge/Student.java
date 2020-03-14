375e608deb4f1910f3038294d3fa227db0aaaabe
Jarod Peachey <jwpeachey107@aol.com>
2020-03-14 15:23:21 -0400

    16.05 Challenge work

---
+++ Module 16/Assignments/16.05 Challenge/Student.java
@@ -0,0 +1,102 @@
+
+/**
+ *
+ *
+ * @author < your name >
+ * @version < today's date >
+ */
+
+import java.util.ArrayList;
+import java.text.DecimalFormat;
+
+public class Student {
+  // instance variables
+  private String name;
+  private ArrayList<Double> scores = new ArrayList<Double>();
+
+  // Constructor for objects of class Student
+  public Student(String name, double scoreOne, double scoreTwo, double scoreThree, double scoreFour, double scoreFive) {
+    // initialize instance variables
+    this.name = name;
+
+    scores.add(scoreOne);
+    scores.add(scoreTwo);
+    scores.add(scoreThree);
+    scores.add(scoreFour);
+    scores.add(scoreFive);
+  }
+
+  // Constuctor that accepts ArrayList of scores
+  public Student(String name, ArrayList<Double> scores) {
+    // initialize instance variables
+    this.name = name;
+
+    this.scores = scores;
+  }
+
+  // Constuctor that accepts array of scores
+  public Student(String name, Double[] scores) {
+    // initialize instance variables
+    this.name = name;
+
+    for (int i = 0; i < scores.length; i++) {
+      this.scores.add(scores[i]);
+    }
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String n) {
+    name = n;
+  }
+
+  public Double getScore(int number) {
+    for (int i = 0; i < scores.size(); i++) {
+      if (i == number) {
+        return scores.get(number);
+      }
+    }
+
+    return 0.0;
+  }
+
+  public void setScore(int number, double score) {
+    scores.set(number - 1, score);
+  }
+
+  public void updateQuiz(int number, double score) {
+    for (int i = 0; i < scores.size(); i++) {
+      if (i == number) {
+        scores.set(i, score);
+      }
+    }
+  }
+
+  public double getAverage() {
+    double total = 0.0;
+    int count = 0;
+    double average = 0.0;
+
+    for (int i = 0; i < scores.size(); i++) {
+      total += scores.get(i);
+      count++;
+    }
+
+    average = total / count;
+
+    return average;
+  }
+
+  public void printResults() {
+    DecimalFormat df = new DecimalFormat("0.##");
+    String numbersString = "";
+
+    for (int i = 0; i < scores.size(); i++) {
+      numbersString += "   " + df.format(scores.get(i));
+    }
+    System.out.printf("%-16s| %5s  | %5s  | %5s  | %5s  | %5s  |    %-5s   |\n", name, scores.get(0), scores.get(1),
+        scores.get(2), scores.get(3), scores.get(4), df.format(getAverage()));
+  }
+}
---
+++ Module 16/Assignments/16.05 Challenge/StudentTest.java
@@ -0,0 +1,198 @@
+
+/**
+ *
+ *
+ * @author < your name >
+ * @version < today's date >
+ */
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.text.DecimalFormat;
+
+public class StudentTest {
+  public static ArrayList<Student> students = new ArrayList<Student>();
+
+  public static void main(String[] args) {
+    students.add(new Student("James White", new Double[] { 88.4, 97.0, 56.5, 45.7, 98.4 }));
+    students.add(new Student("James Brown", new Double[] { 67.4, 97.7, 56.5, 87.4, 82.5 }));
+    students.add(new Student("James Black", new Double[] { 93.5, 85.7, 9.41, 96.5, 72.0 }));
+    students.add(new Student("James Green", new Double[] { 84.4, 75.7, 92.0, 100.0, 86.5 }));
+    students.add(new Student("James Gray", new Double[] { 76.4, 82.0, 65.5, 83.4, 97.7 }));
+
+    printAllData();
+
+    // updateName("James Black", "James Smith");
+    // updateQuiz("James Black", 5, 87.5);
+    // replaceStudent("James Black", new Student("James Smith", new Double[] { 88.4, 97.0, 56.5, 45.7, 98.4 }));
+
+    // addStudent(6, "James Smith", new Double[] { 88.4, 97.0, 56.5, 45.7, 98.4 });
+    // addStudent("James Black", "James Smith", new Double[] { 88.4, 97.0, 56.5,
+    // 45.7, 98.4 });
+    // addStudent("James Smith", new Double[] { 88.4, 97.0, 56.5, 45.7, 98.4 });
+
+    // removeStudent(0);
+    // removeStudent("James Black");
+
+    printAllData();
+  }
+
+  /*
+   * PRINT METHODS FOR CANDIDATES
+   ********************************/
+
+  public static void printAllData() {
+    System.out.println();
+    System.out.println("Gradebook:");
+    System.out.println();
+    printQuizResults();
+    System.out.println();
+  }
+
+  // public static void printStudents() {
+  // for (int i = 0; i < students.size(); i++) {
+  // System.out.println(students.get(i).toString());
+  // }
+  // }
+
+  public static void printQuizResults() {
+    DecimalFormat df = new DecimalFormat("0.##");
+
+    System.out.printf("%-16s|  %3s   |  %3s   |  %3s   |  %3s   |  %3s   |    %3s    |\n", "Student", "Q1", "Q2", "Q3",
+        "Q4", "Q5", "Avg.");
+    System.out.println("----------------+--------+--------+--------+--------+--------+    --------+");
+    for (int i = 0; i < students.size(); i++) {
+      students.get(i).printResults();
+      System.out.println("----------------+--------+--------+--------+--------+--------+    --------+");
+    }
+  }
+
+  // /*
+  // * GETTER METHODS FOR CANDIDATES
+  // ********************************/
+
+  // public static int getTotalVotes() {
+  // int totalVotes = 0;
+
+  // for (int i = 0; i < students.size(); i++) {
+  // totalVotes += students.get(i).getVotes();
+  // }
+
+  // return totalVotes;
+  // }
+
+  // public static double getPercentage(int divisor, int dividend) {
+  // double percentage = ((double) dividend / (double) divisor) * 100;
+
+  // return percentage;
+  // }
+
+  /*
+   * ADD METHODS FOR CANDIDATES
+   ********************************/
+  public static void addStudent(int position, String name, Double[] scores) {
+    if (position > students.size()) {
+      System.out.println("<< Adding " + name + " in position " + students.size() + " >>");
+      students.add(new Student(name, scores));
+    } else {
+      System.out.println("<< Adding " + name + " in position " + position + " >>");
+      students.add(position, new Student(name, scores));
+    }
+  }
+
+  public static void addStudent(String nameSearch, String name, Double[] scores) {
+    boolean foundMatch = false;
+    int index = 0;
+
+    for (int i = 0; i < students.size(); i++) {
+      if (students.get(i).getName() == nameSearch) {
+        foundMatch = true;
+        index = i;
+      }
+    }
+    if (foundMatch) {
+      System.out.println("<< Adding " + name + " before " + nameSearch + " >>");
+      students.add(index, new Student(name, scores));
+    } else {
+      System.out
+          .println("<< Failed adding " + name + ": could not find student " + nameSearch + " to insert before. >>");
+    }
+  }
+
+  public static void addStudent(String name, Double[] scores) {
+    System.out.println("<< Adding " + name + " in position " + students.size() + " >>");
+    students.add(new Student(name, scores));
+  }
+
+  /*
+   * DELETE METHODS FOR CANDIDATES
+   ********************************/
+  public static void removeStudent(int position) {
+    if (position > students.size()) {
+      System.out.println("<< Error removing a student from position " + position + ": that student does not exist. >>");
+    } else {
+      System.out.println(
+          "<< Removing the student, " + students.get(position).getName() + ", from position " + position + " >>");
+
+      students.remove(position);
+    }
+  }
+
+  public static void removeStudent(String nameSearch) {
+    boolean foundMatch = false;
+    int index = 0;
+
+    for (int i = 0; i < students.size(); i++) {
+      if (students.get(i).getName() == nameSearch) {
+        foundMatch = true;
+        index = i;
+      }
+    }
+    if (foundMatch) {
+      System.out
+          .println("<< Removing " + students.get(index).getName() + ", in position " + index + ", from students. >>");
+
+      students.remove(index);
+
+    } else {
+      System.out.println("<< Failed removing student: " + " could not find student " + nameSearch + ". >>");
+    }
+  }
+
+  /*
+   * UPDATE METHODS FOR CANDIDATES
+   ********************************/
+  public static void updateName(String name, String replace) {
+    for (int i = 0; i < students.size(); i++) {
+      if (students.get(i).getName() == name) {
+        System.out.println("<< Changing " + name + " to " + replace + " >>");
+        students.get(i).setName(replace);
+      }
+    }
+  }
+
+  public static void updateQuiz(String name, int number, double score) {
+    for (int i = 0; i < students.size(); i++) {
+      if (students.get(i).getName() == name) {
+        if (number <= 5 && number >= 1) {
+          System.out.println("<< Changing " + name + "'s" + " score for quiz #" + number + " to " + score + " >>");
+          students.get(i).setScore(number, score);
+        } else {
+          System.out.println("<< Cannot update " + name + "'s" + "score for quiz #" + number
+              + " because that quiz does not exist. " + " >>");
+        }
+
+      }
+    }
+  }
+
+  public static void replaceStudent(String name, Student newStudent) {
+    for (int i = 0; i < students.size(); i++) {
+      if (students.get(i).getName() == name) {
+        System.out.println("<< Replacing " + name + " with " + newStudent.getName() + " >>");
+        students.set(i, newStudent);
+      }
+    }
+  }
+}
